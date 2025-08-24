13a7f01ef9bd55793a1188b6a8026546b0e9cea6
achrafkh5 <am.khoualdi@ensta.edu.dz>
2025-08-24 20:22:33 +0100

    google

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+        ],
+      },
+      {
+        source: '/sitemap-:slug.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
