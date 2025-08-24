c2c4db537d48696cab539665ffa2685f56dc1065
achrafkh5 <am.khoualdi@ensta.edu.dz>
2025-08-24 18:17:18 +0100

    google

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://quick-menu.vercel.app', // your live site URL
+  generateRobotsTxt: true, // also generate robots.txt
+};
